d5b777d738e32bed4d0325eb9df3dec65d30328f
Arfan Shaikh <arfanshaikh1@gmail.com>
2021-04-17 23:52:41 +0530

    App for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -19,7 +19,7 @@ weatherForm.addEventListener('submit', (event) => {
     messageOne.textContent = 'Loading';
     messageTwo.textContent = '';
     const location = search.value;
-    fetch('http://localhost:3000/weather?address=' + location)
+    fetch('/weather?address=' + location)
         .then(response => {
             response.json()
                 .then(data => {
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geoCode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define paths for Express config
 const viewsPath = path.join(__dirname, '../templates/views');
@@ -93,6 +94,6 @@ app.get('*',(req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 })
